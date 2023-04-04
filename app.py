b04b0ab95162b44d66d5f2307fcc9717a195157a
KZoubek <91133180+KZoubek@users.noreply.github.com>
2023-04-04 13:11:42 +0200

    design done, flask rebuild WIP

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
-from flask import Flask, render_template
-from flask_navigation import Navigation
+from flask import Flask, render_template, request
+#from flask_wtf import Flaskform
+#from wtforms import Stringfield, Submitfield
 
 app = Flask(__name__)
-nav = Navigation(app)
+#app.config['SECRET_KEY'] = 'mysecret'
 
+@app.route('/')
+def index():
+    #### Return a rendered index.html file
+    return render_template("index.html")
+
+"""
+@app.route('/upload')
+def upload():
+    return render_template("upload.html")
+
+
+@app.route('/analysis')
+def analysis():
+    return render_template("analysis.html")
+"""
+
+if __name__ == '__app__':
+    app.debug = True
+    app.run()
--- app/models.py
+++ app/models.py
@@ -1,16 +1,14 @@
 # Defines app models and routing
-
+"""
 @app.route('/')
 def index():
     #### Return a rendered index.html file
     return render_template("index.html")
 
-@app.route('/navpage')
-def navpage():
-    return render_template('navpage.html')
 @app.route('/upload')
 def analysis():
     return render_template("upload.html")
 @app.route('/analysis')
 def analysis():
     return render_template("analysis.html")
+"""
